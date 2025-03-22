1c37bfc771532686dd770dc6221e67320fc9e3a1
Steve Walton <37951256+SteveBWalton@users.noreply.github.com>
2025-03-22 08:38:10 +0000

    Update comments.

--- season.py
+++ season.py
@@ -69,9 +69,7 @@ class Season:
 
 
     def toHtml(self):
-        '''
-        Returns the season name in html format.
-        '''
+        ''' Returns the season name in html format. '''
         # Add a link.
         html = '<a href="app:home?season={}">'.format(self.index)
 
